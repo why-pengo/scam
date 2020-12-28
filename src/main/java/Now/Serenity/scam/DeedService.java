ffed7c5bb8457c2b98f6054ccd1b13cf55a97855
Jon Morgan <morgan.jon@gmail.com>
2020-12-28 14:29:54 -0500

    got listing

--- src/main/java/Now/Serenity/scam/DeedService.java
+++ src/main/java/Now/Serenity/scam/DeedService.java
@@ -3,6 +3,7 @@ package Now.Serenity.scam;
 import org.apache.commons.io.FileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.io.File;
@@ -12,10 +13,13 @@ import java.util.Collection;
 public class DeedService {
     private static final Logger logger = LoggerFactory.getLogger(DeedService.class);
 
+    @Value("${base-dir}")
+    private String baseDir;
+
     public Collection<File> listFiles() {
-        File baseDir = new File("/home/cams/videos");
-        Collection<File> files = FileUtils.listFiles(baseDir, new String[]{"jpg", "mp4"}, true);
-        files.stream().forEach((file -> logger.info(file.toString())));
+        File listDir = new File(baseDir);
+        Collection<File> files = FileUtils.listFiles(listDir, new String[]{"jpg", "mp4"}, true);
+        files.forEach((file -> logger.info(file.toString())));
 
         return files;
     }
--- src/test/java/Now/Serenity/scam/DeedServiceTest.java
+++ src/test/java/Now/Serenity/scam/DeedServiceTest.java
@@ -3,6 +3,8 @@ package Now.Serenity.scam;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.io.File;
 import java.util.Collection;
@@ -10,6 +12,7 @@ import java.util.Collection;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
+@ActiveProfiles("dev")
 class DeedServiceTest {
 
     @Autowired
