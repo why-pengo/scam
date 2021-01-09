2f8568e7d20938d4719c53bc205d63b6fb0c52ef
Jon Morgan <morgan.jon@gmail.com>
2021-01-09 17:54:03 -0500

    switch to RestController

--- src/main/java/Now/Serenity/scam/controllers/ApiController.java
+++ src/main/java/Now/Serenity/scam/controllers/ApiController.java
@@ -3,21 +3,22 @@ package Now.Serenity.scam.controllers;
 import Now.Serenity.scam.services.DeedService;
 import Now.Serenity.scam.domain.Deed;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.io.File;
 import java.util.Collection;
 import java.util.List;
 
-@Controller
-@RequestMapping(value = "/api")
+@RestController
+@RequestMapping("api")
 public class ApiController {
 
     @Autowired
     DeedService deedService;
 
-    @RequestMapping(value = "/listDeeds")
+    @GetMapping(value = "/listDeeds")
     public List<Deed> listDeeds() {
         Collection<File> files = deedService.readFileSystem();
         return deedService.parseFileSystemToDeeds(files);
