f2d9764a109d2661a415a7305556171ceed19526
Jon Morgan <morgan.jon@gmail.com>
2021-07-11 20:56:01 -0400

    working on HSTS issues in dev with Self-signed cert

--- src/main/java/Now/Serenity/scam/controllers/LoginController.java
+++ src/main/java/Now/Serenity/scam/controllers/LoginController.java
@@ -1,12 +1,20 @@
 package Now.Serenity.scam.controllers;
 
+import Now.Serenity.scam.services.DeedService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class LoginController {
+    private static final Logger logger = LoggerFactory.getLogger(LoginController.class);
+
     @RequestMapping(value = "/login")
+    @CrossOrigin(origins = "https://localhost:8443/")
     public String login() {
+        logger.info("login called.");
         return "login";
     }
 }
--- src/main/java/Now/Serenity/scam/security/WebSecurityConfig.java
+++ src/main/java/Now/Serenity/scam/security/WebSecurityConfig.java
@@ -16,6 +16,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+//                .cors().and().csrf().disable()
             .authorizeRequests()
                 .antMatchers("/").permitAll()
                 .antMatchers("/listDeeds").permitAll()
